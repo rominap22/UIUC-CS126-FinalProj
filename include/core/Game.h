6f38ad3eb2294e99c6e14401e837d8713704bcfd
rominap22 <romi.parimi@gmail.com>
2021-05-03 01:43:11 -0500

    Finished comments for Game.h and Player.h.

--- include/core/Game.h
+++ include/core/Game.h
@@ -6,22 +6,84 @@
 namespace garbage {
     class Game {
     public:
+        //for setting a game of any number of at least 2 players
         Game(size_t num_players);
         Game();
+        //for setting a game of any number of at least 2 players
+        //plus generating a seed for a random sequence of #'s for each game
         Game(size_t num_players, size_t seed);
         ~Game();
+        /**
+         * Prints the board for the passed in player
+         * @param player_num the player we are referring to (e.g. Player 1)
+         * @return printed player's hand as a string
+         */
         string to_string(size_t player_num);
+        /**
+         * Prints the board for any player
+         * @return printed player's hand as a string
+         */
         string to_string();
+        /**
+         * Gets the summary of each player's hand on the screen.
+         * @param hand list of 10 cards dealt to the player that constitutes their "board"
+         * @param player_num the player we are referring to (e.g. Player 1)
+         */
         void get_summary(Card* hand, size_t player_num);
+        /**
+         * Plays one iteration of the game with correctly stepping, placing jacks
+         * as necessary by selecting best rank for jack
+         */
         void play_game();
+        /**
+         * Takes one turn of the game for a player (call's board's turn function)
+         * @return true if turn is over, false otherwise (card unplayable or player won)
+         */
         bool turn();
+        /**
+         * Takes one step of the game for a player's turn (call's board's step function)
+         * @return true if player has won, false otherwise
+         */
         bool step();
+        /**
+         * Checks if game is over (call's board's is_over function)
+         * @return true if the game is over, false otherwise
+         */
         bool is_over();
+        /**
+         * Prints state of game (game summary) as a string w/ each player's hands
+         * (calls board's game_summary function)
+         * @return outputted player's hands and playable cards as a string
+         */
         string game_summary();
+        /**
+         * Checks if the card is a jack (call's board's is_jack function)
+         * @return true if card is jack, false if not
+         */
         bool is_jack();
+        /**
+         * Places jack at the position of the passed in rank and replaces
+         * the face down card at that position with jack
+         * @param rank number of the card to be replaced by Jack
+         */
         void place_jack(size_t rank);
+        /**
+         * Checks if rank is a valid rank - card should be face down
+         * and player has not won yet
+         * @param rank number of the card to be checked
+         * @return true if the rank is valid based on above conditions
+         */
         bool is_rank_good(size_t rank);
+        /**
+         * Selects best rank based on board's function for select_best_rank
+         * @return optimal rank based on method from board
+         */
         size_t select_best_rank();
+        /**
+         * Gets the last added card to the discard pile based on board's function
+         * for get_last_discard
+         * @return Card last added to discard pile
+         */
         Card get_last_discard();
     private:
         Board* board;
--- include/core/Player.h
+++ include/core/Player.h
@@ -9,19 +9,87 @@ namespace garbage {
     class Player {
     public:
         Player();
+        //setting each player's name and hand
         Player(string name, Board* board);
+        /**
+         * Adds a card to a player's hand
+         * @param card Card to be added to player's hand
+         */
         void add_card(Card card);
+        /**
+         * For Cinder app:
+         * Printing out if player has won, card to be played, or if the turn is over
+         * and could not play a card, or if turn is over after sequence of played cards
+         * @param out output from state of game based on played/unplayed cards
+         */
         void print(ostream& out);
+        /**
+         * For console:
+         * Printing out if player has won, card to be played, or if the turn is over
+         * and could not play a card, or if turn is over after sequence of played cards
+         * @param out output from state of game based on played/unplayed cards
+         * @param out
+         */
         void print_summary(ostream& out);
+        /**
+         * Takes a turn for a player by getting the rank, skipping over any card that
+         * is not a Jack and face up, swaps drawn card with card on the board at that rank,
+         * set card to face up, add replaced card to discard pile
+         */
         void turn();
+        /**
+         * Gets the name of the player
+         * @return name of player as a string
+         */
         string get_name();
+        /**
+         * Checks if the passed in rank in the hand is face up
+         * @param rank number of the card to be checked
+         * @return true if card of that rank is face up, false if not
+         */
         bool is_face_up(size_t rank);
+        /**
+         * Iterates over entire player's hand to ensure that all cards are face up
+         * which means that game is over because that player won
+         * @return true if all cards are face up in a hand, false otherwise
+         */
         bool is_game_over();
+        /**
+         * Takes each intermediate step of a player's turn by getting rank of card,
+         * asking for rank to replace if Jack, replace card on the board with drawn
+         * or user-chosen card of that rank, sets card to face up, adds drawn card to discard,
+         * checks if game is over or unplayable card
+         * @return true if intermediate step is over and card successfully replaced,
+         * false otherwise
+         */
         bool step();
+        /**
+         * Checks if card is playable and draws the next card to be played
+         * @return Card that should next be drawn from stock pile
+         */
         Card next_card();
+        /**
+         * Check if card is a Jack
+         * @return true if card is a jack, false if not
+         */
         bool is_jack();
+        /**
+         * Replaces the card on the board at the passed in rank with Jack
+         * @param rank number of card to be checked
+         */
         void place_jack(size_t rank);
+        /**
+         * Checks if passed in rank is valid - card should be face down,
+         * player should not have won yet
+         * @param rank number of card to be checked
+         * @return true if rank is valid based on above conditions, false otherwise
+         */
         bool is_rank_good(size_t rank);
+        /**
+         * Gets the summary of the state of the game in terms of the player's hand,
+         * card to be played if playable (or an empty Card obj if not playable)
+         * @param hand list of 10 cards dealt to the player that constitutes their "board"
+         */
         void get_summary(Card* hand);
     private:
         string name;
